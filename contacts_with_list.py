f18a21850db45bc9b588ceaa3315bff7e7ba87e3
khind <kevin.hind@proquest.com>
2022-01-25 12:24:04 +0000

    Refactored following session with Dan Bye.

--- contacts_with_list.py
+++ contacts_with_list.py
@@ -7,16 +7,10 @@
 # Sort contacts: Sort the keys in the dictionary
 # How can the contacts list be saved for later (i.e. to avoid having to add them all again?)
 
-exit = False
-
-# contacts = [{
-#     'first_name' : 'Kevin',
-#     'last_name' : 'Hind',
-#     'full_name' : 'Hind, Kevin',
-#     'company' : 'Clarivate',
-#     'phone' : '(01223) 271259',
-#     'e-mail' : 'kevin.hind@proquest.com'
-#     }]
+# Take a case-insensitive string - use this to search the contacts database
+# e.g. 'proquest' - search anything where this is mentioned in company or e-mail address
+# Enable functionality to display a specific contact
+# List first name, last name, e-mail address
 
 
 def menu_of_options():
@@ -26,9 +20,10 @@ def menu_of_options():
         1 - Print the menu
         2 - Print the contacts list
         3 - Add a contact
-        4 - Delete or Edit a contact by name
-        5 - Save the file
-        6 - Sort the contacts
+        4 - Edit a contact by name
+        5 - Delete a contact by name
+        6 - Save the file
+        7 - Sort the contacts
         0 - Exit
         """)
 
@@ -51,61 +46,60 @@ def add_a_contact(contacts):
     return contacts
 
 
-def edit_or_delete_contact(contacts):
-    # This mixes presentation with logic
-    # Should separate user input functions from functions that perform actions on the list
-    # The menu should be envisaged as a separate component
+def delete_contact(contacts):
     to_edit = input(
             """Enter the name of a contact
             in the form last name, first_name: """).strip()
-    action = input(
-        """
-        Select from one of the following options:
-        To delete a contact, type '1'
-        To edit a contact, type '2'
-        : """)
+
+    contact_to_delete = filter_contact(contacts, 'full_name', to_edit)[0]
+    contacts.remove(contact_to_delete)
+    save_the_contacts(contacts)
+
+def edit_contact(contacts):
+    to_edit = input(
+            """Enter the name of a contact
+            in the form last name, first_name: """).strip()
+
+    contact_to_edit = filter_contact(contacts, 'full_name', to_edit)[0]
+
+
     while True:
-        if action == '1':
-            contact_to_delete = filter_contact(contacts, 'full_name', to_edit)[0]
-            contacts.remove(contact_to_delete)
-            save_the_contacts(contacts)
+        selection = input(
+        """
+        Which contact field do you want to edit?
+        Select from:
+        1 - First Name
+        2 - Last Name
+        3 - Company
+        4 - Phone
+        5 - E-mail
+        : """
+        )
+
+        if selection == '1':
+            field_to_edit = "first_name"
             break
-        elif action == '2':
-            contact_to_edit = filter_contact(contacts, 'full_name', to_edit)[0]
-            contacts = edit_contact(contacts, contact_to_edit)
-            save_the_contacts(contacts)
+        elif selection == '2':
+            field_to_edit = "last_name"
+            break
+        elif selection == '3':
+            field_to_edit = "company"
+            break
+        elif selection == '4':
+            field_to_edit = "phone"
+            break
+        elif selection == '5':
+            field_to_edit = "e-mail"
             break
         else:
-            print("Unknown action.  Try again")
+            "Please enter a number from the menu."
 
-
-def edit_contact(contacts, contact_to_edit):
-    selection = input(
-    """
-    Which contact field do you want to edit?
-    Select from: first_name, last_name, company, phone, e-mail
-    To select 'first_name', type '1'
-    To select 'last_name', type '2'
-    To select 'company', type '3'
-    To select 'phone', type '4'
-    To select 'e-mail', type '5'
-    : """
-    )
-
-    number_to_field = {
-        '1' : 'first_name',
-        '2' : 'last_name',
-        '3' : 'company',
-        '4' : 'phone',
-        '5' : 'e-mail'
-    }
-
-    field_to_edit = number_to_field[selection]
 
     new_value = input("Type replacement text for this field: ")
     contact_to_edit[field_to_edit] = new_value
     if field_to_edit in ['first_name', 'last_name']:
         contact_to_edit['full_name'] = contact_to_edit['last_name'] + ', ' + contact_to_edit['first_name']
+    save_the_contacts(contacts)
     return contacts
 
 
@@ -114,7 +108,7 @@ def filter_contact(contacts, field, value):
 
 
 def save_the_contacts(contacts):
-    with open('contacts_output.txt', 'wt') as output_file:
+    with open('contacts.txt', 'wt') as output_file:
         for contact in contacts:
             for key, value in contact.items():
                 if key != 'e-mail':
@@ -124,36 +118,32 @@ def save_the_contacts(contacts):
             output_file.write('\n')
 
 
-with open('file_to_read.txt', 'rt') as input_file:
-    individual_contacts = [line.strip() for line in input_file.readlines()]
+with open('contacts.txt', 'rt') as input_file:
     contacts = []
-    for individual_contact in individual_contacts:
-        contact = {}
-        for contact_detail in individual_contact.split('|'):
-            contact[contact_detail.split(':')[0]] = contact_detail.split(':')[1]
-        contacts.append(contact)
+    for contact in [line.strip() for line in input_file]:
+        contacts.append(dict([tuple(i.split(':')) for i in contact.split('|')]))
 
 
 while True:
     # Where user is required to type words, don't mix case
     # use numbers for basic options
-    print(menu_of_options())
+    menu_of_options()
     selection = input("Enter a number from the menu: ")
 
     if selection == '1':
-        print(menu_of_options())
+        menu_of_options()
     elif selection == '2':
         print(contacts)
     elif selection == '3':
-        contacts = add_a_contact(contacts)
+        add_a_contact(contacts)
     elif selection == '4':
-        edit_or_delete_contact(contacts)
+        edit_contact(contacts)
     elif selection == '5':
-        save_the_contacts(contacts)
+        delete_contact(contacts)
     elif selection == '6':
-        contacts = sorted(contacts, key = lambda i: i['last_name'])
-    # elif selection == '6':
-    #     print(contacts.keys())
+        save_the_contacts(contacts)
+    elif selection == '7':
+        contacts.sort(key = lambda i: i['last_name'])
     elif selection == '0':
         break
     else:
