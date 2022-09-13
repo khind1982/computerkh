83c71418c39483bfe17259a83ed73f9da27690d4
khind <kevinhind2000@yahoo.co.uk>
2022-09-13 22:04:25 +0100

    Code to build employee table from database in django and style it.

--- classic_cars/get_employee_data/views.py
+++ classic_cars/get_employee_data/views.py
@@ -1,8 +1,16 @@
+import os
+import sqlite3
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 # names = {'name' : 'Kevin'}
+db = sqlite3.connect("/home/khind/git/computerkh/db/classic_models.db")
+cursor = db.cursor()
+cursor.execute("SELECT * FROM employees")
+employees = [i for i in cursor]
 
 def say_hello(request):
     return render(request, 'mypage.html')
@@ -10,4 +18,4 @@ def say_hello(request):
 # Any string-based dataset
 # Could read in data from a DB
 def retrieve_employee_data(request):
-    return render(request, 'employee_tracker.html', {'name':'Kevin'})
+    return render(request, 'employee_tracker.html', {'data': employees})
